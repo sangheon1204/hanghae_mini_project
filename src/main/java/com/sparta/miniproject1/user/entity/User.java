9e3f17900853c0e58b00fdaf5d1080b531c62239
kjk0122 <juke9548@gmail.com>
2023-01-13 14:04:07 +0900

    Gradle dependency 추가 및 유저 파일 추가

---
+++ src/main/java/com/sparta/miniproject1/user/entity/User.java
@@ -0,0 +1,41 @@
+package com.sparta.miniproject1.user.entity;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Pattern;
+
+@Getter
+@NoArgsConstructor
+@Entity(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // nullable: null 허용 여부
+    // unique: 중복 허용 여부 (false 일때 중복 허용)
+
+    @Column(nullable = false)
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-z]).{4,10}")
+    private String username;
+
+
+    @Column(nullable = false)
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-z]).{4,10}")
+        private String password;
+
+
+    @Enumerated(value = EnumType.STRING)
+    private UserRoleEnum role;
+
+    public User(String username, String password, UserRoleEnum role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+}
---
+++ src/main/java/com/sparta/miniproject1/user/service/UserService.java
@@ -0,0 +1,89 @@
+package com.sparta.miniproject1.user.service;
+
+
+import com.sparta.miniproject1.user.dto.LoginRequestDto;
+import com.sparta.miniproject1.user.dto.SignupRequestDto;
+import com.sparta.miniproject1.user.entity.User;
+import com.sparta.miniproject1.user.entity.UserRoleEnum;
+import com.sparta.miniproject1.user.jwt.JwtUtil;
+import com.sparta.miniproject1.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
+import java.util.regex.Pattern;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    private final JwtUtil jwtUtil;
+    private final PasswordEncoder passwordEncoder;
+    // ADMIN_TOKEN
+    private static final String ADMIN_TOKEN = "AAABnvxRVklrnYxKZ0aHgTBcXukeZygoC";
+    //패턴 체크
+    String pt = "^[a-z\\\\d`~!@#$%^&*()-_=+]{4,10}$"; String ptt = "^[a-zA-Z\\\\d`~!@#$%^&*()-_=+]{8,15}$";
+
+    @Transactional
+    public void signup(SignupRequestDto signupRequestDto) {
+        //이름, 비밀번호 대조를 위해 값을 뽑아놓음
+        String username = signupRequestDto.getUsername();
+        String pwcheck =  signupRequestDto.getPassword();
+
+        //username 확인
+        if(!Pattern.matches(pt, username)){
+            throw new IllegalArgumentException(
+                    "아이디는 최소 4자 이상, 10자 이하이며 알파벳 소문자(a~z), 숫자(0~9)로 되어야합니다.");
+        }
+        //비밀번호 확인
+        if(!Pattern.matches(ptt, pwcheck)){
+            throw new IllegalArgumentException(
+                    "비밀번호는 최소 8자 이상, 15자 이하이며 알파벳 대소문자(a~z, A~Z), 숫자(0~9), 특수문자로 되어야합니다.");
+
+        }
+        // 회원 중복 확인
+        Optional<User> found = userRepository.findByUsername(username);
+        if (found.isPresent()) {
+            throw new IllegalArgumentException("중복된 사용자가 존재합니다.");
+        }
+        //역할군 지정 위해서 role 변수 만듦 + 초기값은 USER로
+        UserRoleEnum role = UserRoleEnum.USER;
+        //토큰 확인!
+        if (signupRequestDto.isAdmin()) {
+            if (!signupRequestDto.getAdminToken().equals(ADMIN_TOKEN)) {
+                throw new IllegalArgumentException("관리자 암호가 틀려 등록이 불가능합니다.");
+            }
+            role = UserRoleEnum.ADMIN;
+        }
+
+        //저장을 바로 하면 안되고 encoding해서 저장하기
+        String password = passwordEncoder.encode(pwcheck);
+
+        //등록등록
+        User user = new User(username, password, role);
+        userRepository.save(user);
+    }
+
+
+    @Transactional(readOnly = true)
+    public void login(LoginRequestDto loginRequestDto, HttpServletResponse response) {
+        //이름, 비밀번호 대조를 위해 값을 뽑아놓음
+        String username = loginRequestDto.getUsername();
+        String password = loginRequestDto.getPassword();
+
+        // 사용자 확인
+        User user = userRepository.findByUsername(username).orElseThrow(
+                () -> new IllegalArgumentException("등록된 사용자가 없습니다.")
+        );
+
+        // 비밀번호 확인
+        if(!passwordEncoder.matches(password, user.getPassword())){
+            throw  new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+        //토큰을 생성해서 유저에게 줌
+        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(user.getUsername(), user.getRole()));
+    }
+}
