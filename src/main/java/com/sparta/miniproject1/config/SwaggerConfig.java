5b72a707c10082eb11160bcd1b9cddfe6d04f614
SANGHEON LEE <mongsil1204@naver.com>
2023-01-28 10:03:52 +0900

    코드 정리 #1

--- src/main/java/com/sparta/miniproject1/config/SwaggerConfig.java
+++ src/main/java/com/sparta/miniproject1/config/SwaggerConfig.java
@@ -3,20 +3,14 @@ package com.sparta.miniproject1.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContext;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.Arrays;
-import java.util.List;
 
 
 @EnableSwagger2
